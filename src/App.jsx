177bf0fb37b60e65997057b4ea5ce3c871337093
Gunasekaranuppur <m.gunasekaran1998@gamil.com>
2026-09-08 17:05:29 +0530

    add admin

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,27 @@ import './App.css'
 import Footer from './component/Footer'
 import Heater from './component/Heater'
 import Home from './pages/Home'
+import ProtectedRoute from './component/ProtectedRoute'
 
 function App() {
 
 
   return (
     <>
-    <Heater/>
- <Routes>
+      <Heater />
+      <Routes>
         <Route path="/" element={<Home />} />
-     
+
+
+  <Route path="admin" element={<ProtectedRoute />}>
+    <Route path="dashboard" element={<Dashboard />} />
+    <Route path="profile" element={<Profile />} />
+    <Route path="orders" element={<Orders />} />
+    <Route path="settings" element={<Settings />} />
+  </Route>
       </Routes>
-    <Footer/>
-   
+      <Footer />
+
     </>
   )
 }
