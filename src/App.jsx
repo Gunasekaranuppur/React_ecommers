b746c0ab2ba363567a750b076eaa50136a72765a
Gunasekaranuppur <m.gunasekaran1998@gamil.com>
2026-09-08 18:37:52 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import Footer from './component/Footer'
 import Heater from './component/Heater'
 import Home from './pages/Home'
-import ProtectedRoute from './component/ProtectedRoute'
+// import ProtectedRoute from './component/ProtectedRoute'
 
 function App() {
 
@@ -17,12 +17,12 @@ function App() {
         <Route path="/" element={<Home />} />
 
 
-  <Route path="admin" element={<ProtectedRoute />}>
+  {/* <Route path="admin" element={<ProtectedRoute />}>
     <Route path="dashboard" element={<Dashboard />} />
     <Route path="profile" element={<Profile />} />
     <Route path="orders" element={<Orders />} />
     <Route path="settings" element={<Settings />} />
-  </Route>
+  </Route> */}
       </Routes>
       <Footer />
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,45 @@
-
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 function Home() {
+ const [index, setIndex] = useState(0);
 
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
 
-  return (
-    <>
-   <h1> this home page  </h1>
-   
-    </>
-  )
+    return (
+        <>
+            {/* baner start */}
+            <section className='baner'>
+                <Carousel>
+                    <Carousel.Item>
+                        <img src="/images/slider-1.jpg" text="First slide" />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                          <img src="/images/slider-2.jfif" text="Second slide" />
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                          <img src="/images/slider-3.jpg" text="Third slide" />
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </section>
+            {/* baner end  */}
+        </>
+    )
 }
 
 export default Home
